8efc7d1ba1c46ca6763c0093c3a9f6b638c85343
Maki Spariva Olona <110998002+spariva@users.noreply.github.com>
2023-09-28 19:52:33 +0200

    Update main,.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,13 @@
-const DAW = document.getElementById('asignaturas').textContent; 
-/*
-Falta el onclick y depende cómo lo hagamos necesitaremos estas constantes:
-
-const DWES = document.getElementById('').textContent;
-const DIW = document.getElementById('').textContent;
-const DWEC = document.getElementById('').textContent;
-const EIE = document.getElementById('').textContent;
-const ITGS = document.getElementById('').textContent;
-
-const textoOriginal = document.getElementById('').textContent;
-*/
-
+const DAW = document.getElementById('DAW').textContent;
+const DWES = document.getElementById('DWES').textContent;
+const DIW = document.getElementById('DIW').textContent;
+const DWEC = document.getElementById('DWEC').textContent;
+const EIE = document.getElementById('EIE').textContent;
+const ITGS = document.getElementById('ITGS').textContent; 
 
 function agregarMas(){
     // Obtener el texto actual textContent es como innerhtml
     let textoActual = DAW.textContent;
-    // Agregar el símbolo "+" al texto
     textoActual += '+';
     DAW.textContent = textoActual;
 }
-
-
-// Agregar un evento de clic al botón. En caso de no onclick.
-agregarButton.addEventListener('click', agregarMas);
